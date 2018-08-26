1a72b3c3e347d2560320dc0df3abcdd6127d3364
DChang <chang19d@gmail.com>
2018-08-26 00:59:33 -0400

    added things

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,8 +36,72 @@ var q5 = {question:"During the 1980s for six consecutive years what breed of dog
           d:"Poodle",
           answer:"a"
 };
+var q6 = {question:"Who is third behind Hank Aaron and Babe Ruth in major league career home runs?", 
+          a:"Reggie Jackson", 
+          b:"Harmon Killebrew",
+          c:"Willie Mays",
+          d:"Frank Robinson",
+          answer:"c"
+};
+var q7 = {question:"In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?", 
+          a:"8%", 
+          b:"18%",
+          c:"38%",
+          d:"58%",
+          answer:"b"
+};
+var q8 = {question:"In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?", 
+          a:"10%", 
+          b:"15%",
+          c:"31%",
+          d:"51%",
+          answer:"c"
+};
+var q9 = {question:"The first black American pictured on a U.S. postage stamp was who?", 
+          a:"Frederick Douglass", 
+          b:"Booker T. Washington",
+          c:"Louis Armstrong",
+          d:"Joe Louis",
+          answer:"d"
+};
+var q10 = {question:"What did the 'D' in 'D-Day' stand for?", 
+          a:"Doom", 
+          b:"Day",
+          c:"Dwight(Eisenhower)",
+          d:"Dunkirk",
+          answer:"b"
+};
+var q11 = {question:"The Brownie Box Camera introduced by Eastman Kodak in 1900 had a retail price of what?", 
+          a:"1$", 
+          b:"5$",
+          c:"10$",
+          d:"20$",
+          answer:"a"
+};
+var q12 = {question:"Which of these characters turned 40 years old in 1990?", 
+          a:"Charlie Brown", 
+          b:"Bugs Bunny",
+          c:"Mickey Mouse",
+          d:"Fred Flintstone",
+          answer:"a"
+};
+var q13 = {question:"When Mt. St. Helens erupted on May 18, 1980, how many people were killed?", 
+          a:"1", 
+          b:"57",
+          c:"143",
+          d:"426",
+          answer:"b"
+};
+var q14 = {question:"In J. Edgar Hoover, what did the J stand for?", 
+          a:"James", 
+          b:"John",
+          c:"Joseph",
+          d:"Jimtavius Maximus",
+          answer:"b"
+};
+
 console.log(q5);
-var questionsArray = [q1,q2,q3,q4,q5];
+var questionsArray = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14];
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 
@@ -94,9 +158,17 @@ var answer3;
 var answer4;
 var userAnswer;
 var correctChoice;
-
+var arrayOfUsedQuestions=[];
     function pickRandomQuestion(){
-        var indexOfQuestion = Math.floor(Math.random() * 5);
+        var indexOfQuestion = Math.floor(Math.random() * 14);
+        
+        for(var i = 0; i<arrayOfUsedQuestions.length; i++){
+            if(arrayOfUsedQuestions[i] == indexOfQuestion){
+                pickRandomQuestion();
+            }
+        }
+        arrayOfUsedQuestions.push(indexOfQuestion);
+        
         questionToAsk = questionsArray[indexOfQuestion].question;
         correctChoice = questionsArray[indexOfQuestion].answer;
         answer1 = questionsArray[indexOfQuestion].a;
@@ -126,7 +198,7 @@ var correctChoice;
     function startGame(){
         
         $questionsDiv.empty();
-        $questionsDiv.html("<div id='display'>30</div>");
+        $questionsDiv.html("<div id='display' style='font-size:400%; margin-top:-8%; margin-bottom:-4%;'>30</div>");
         $questionsDiv.append("<div class = 'smallQuestionsDiv'></div></br>");
         $questionsDiv.append("<div class = 'smallAnswersDiv'></div>");
         
@@ -141,9 +213,9 @@ var correctChoice;
         $(".correctAnswers").text("Correct Answers: " + correctAnswers);
         $questionsDiv.append("<h2 class = 'incorrectAnswers'></h2>");
         $(".incorrectAnswers").text("Incorrect Answers: " + incorrectAnswers);
-        $questionsDiv.append("<h2 class = 'unanswered'></h2>");
-        $(".unanswered").text("Unanswered: " + unanswered);
+      
     }
+
 
     $('body').on('click', '.answer1, .answer2, .answer3, .answer4', function () {
         console.log("Answer: " + $(this).attr("data-answer"));
